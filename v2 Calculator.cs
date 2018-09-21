a0b44c3fedf8da8b6c03ed8f6dfd1bc4ca95c0b8
Daniel123wq <33332606+Daniel123wq@users.noreply.github.com>
2018-09-20 21:52:21 -0300

    Create v2 Calculator.cs

---
+++ v2 Calculator.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    
+    public class Calculadora
+    {
+        double num1,num2,result;
+        char operacao;
+        public void setNum1(double n1)
+        {
+            num1 = n1;
+        }
+        public void setOperacao(char op)
+        {
+            operacao = op;
+        }
+        public void setNum2(double n2)
+        {
+            num2 = n2;
+        }
+        public void setResult()
+        {
+            switch(operacao)
+                {
+                case '*':
+
+                break;
+
+            }
+            result = 
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
